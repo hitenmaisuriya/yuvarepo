c517146f6860cb3f128b41c713d1e958e96d69b5
hitenmaisuriya <h.maisuriya@gmail.com>
2017-03-04 11:14:51 +0530

    --field controller --

---
+++ yuvarepo/yuvarepo/Controllers/fieldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using yuvarepo.Models;
+
+
+namespace yuvarepo.Controllers
+{
+    public class fieldController : Controller
+    {
+        // GET: field
+        public ActionResult Index()
+        {
+            return View();
+        }
+        static fields fieldes_model = new fields();
+        public ActionResult fields_ins(fields obj)
+        {
+            var response = fieldes_model.field_ins(obj);
+            return Json(response,JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult all_fields(fields obj)
+        {
+            var response = fieldes_model.all_field();
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+      
+    }
+}
---
+++ yuvarepo/yuvarepo/Controllers/profileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using yuvarepo.Models;
+namespace yuvarepo.Controllers
+{
+    public class profileController : Controller
+    {
+        // GET: profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        static profile profiles = new profile();
+        public ActionResult profile_ins(profile obj)
+        {
+            var response=0;
+            if (Request.Cookies["bid"] != null)
+            {
+                obj.updated_by = Request.Cookies["bid"].Values.ToString().Substring(4);
+                obj.id = Request.Cookies["bid"].Values.ToString().Substring(4);
+                response = profiles.profile_ins(obj);
+                
+
+            }
+            ViewBag.profile_ins = "Profile Data Saved.";
+            return Json(response, JsonRequestBehavior.AllowGet);
+            //return View("index");
+        }
+    }
+}
